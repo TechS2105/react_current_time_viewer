053226e84aca0a7d6fe41886369839c5bd9c97c0
TechS2105 <techsovan.github0521@gmail.com>
2025-03-20 15:54:27 +0530

    files are modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 function App() {
 
     const currentTime = new Date().toLocaleTimeString(undefined, {
 
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
         hour12: false
 
     });
 
     const [time, setTime] = useState(currentTime);
 
-    function increaseTime() {
-        
+    function handelClick(){
+
         const newTime = new Date().toLocaleTimeString(undefined, {
 
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
             hour12: false
 
         });
@@ -28,14 +28,14 @@ function App() {
 
     }
 
-    setInterval(increaseTime, 1000)
+    setInterval(handelClick, 1000);
     
     return (
 
         <div className="container">
 
-            <h1> {time} </h1>
-            <button onClick={increaseTime}> Get Time </button>
+            <h1>{time}</h1>
+            <button onClick={handelClick}> Get Time </button>
 
         </div>
 
